d9f5521aede5152210d822762814ce5a8b46925d
rducrot <ducrot.romain@gmail.com>
2022-12-19 16:49:57 +0100

    Distinction serializers liste/détail

--- api/serializers.py
+++ api/serializers.py
@@ -3,22 +3,46 @@ from rest_framework import serializers
 from api.models import Project, Issue, Comment
 
 
-class ProjectSerializer(serializers.ModelSerializer):
+class CommentSerializer(serializers.ModelSerializer):
 
     class Meta:
-        model = Project
-        fields = ['title', 'description', 'type', 'contributors']
+        model = Comment
+        fields = ['description', 'author_user', 'issue']
 
 
-class IssueSerializer(serializers.ModelSerializer):
+class IssueListSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = Issue
         fields = ['title', 'description', 'tag', 'priority', 'status', 'project']
 
 
-class CommentSerializer(serializers.ModelSerializer):
+class IssueDetailSerializer(serializers.ModelSerializer):
 
     class Meta:
-        model = Comment
-        fields = ['description', 'author_user', 'issue']
+        model = Issue
+        fields = ['title', 'description', 'tag', 'priority', 'status', 'project', 'comments']
+
+    def get_comments(self, instance):
+        queryset = instance.comments.all()
+        serializer = CommentSerializer(queryset, many=True)
+        return serializer.data
+
+
+class ProjectListSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = Project
+        fields = ['title', 'description', 'type', 'contributors']
+
+
+class ProjectDetailSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = Project
+        fields = ['title', 'description', 'type', 'contributors', 'issues']
+
+    def get_issues(self, instance):
+        queryset = instance.issues.all()
+        serializer = IssueListSerializer(queryset, many=True)
+        return serializer.data
--- api/views.py
+++ api/views.py
@@ -1,26 +1,41 @@
 from rest_framework.viewsets import ModelViewSet
 
 from api.models import Project, Issue, Comment
-from api.serializers import ProjectSerializer, IssueSerializer, CommentSerializer
+from api.serializers import ProjectListSerializer, ProjectDetailSerializer,\
+    IssueListSerializer, IssueDetailSerializer, CommentSerializer
 
 
-class ProjectViewSet(ModelViewSet):
+class MultipleSerializerMixin:
+    detail_serializer_class = None
 
-    serializer_class = ProjectSerializer
+    def get_serializer_class(self):
+        # Notre mixin détermine quel serializer à utiliser
+        # même si elle ne sait pas ce que c'est ni comment l'utiliser
+        if self.action == 'retrieve' and self.detail_serializer_class is not None:
+            # Si l'action demandée est le détail alors nous retournons le serializer de détail
+            return self.detail_serializer_class
+        return super().get_serializer_class()
+
+
+class ProjectViewSet(MultipleSerializerMixin, ModelViewSet):
+
+    serializer_class = ProjectListSerializer
+    detail_serializer_class = ProjectDetailSerializer
 
     def get_queryset(self):
         return Project.objects.all()
 
 
-class IssueViewSet(ModelViewSet):
+class IssueViewSet(MultipleSerializerMixin, ModelViewSet):
 
-    serializer_class = IssueSerializer
+    serializer_class = IssueListSerializer
+    detail_serializer_class = IssueDetailSerializer
 
     def get_queryset(self):
         return Issue.objects.filter(project=self.kwargs['project_pk'])
 
 
-class CommentViewSet(ModelViewSet):
+class CommentViewSet(MultipleSerializerMixin, ModelViewSet):
 
     serializer_class = CommentSerializer
 
